34894659179cd667258cde2658fdc89a29cb6d6d
kassadube <noamsrb@yahoo.com>
2018-03-25 17:38:37 +0300

    check auth when page load

--- src/Root.js
+++ src/Root.js
@@ -3,9 +3,16 @@ import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
 import store, { history } from './store'
 import AppLayout from './components/app_layout'
+import auth from './modules/auth'; 
 
 import 'sanitize.css/sanitize.css';
 import './index.css'
+
+const { AUTH_USER } = auth.actionsTypes;
+const token = localStorage.getItem('token');
+
+if(token) 
+    store.dispatch({ type: AUTH_USER ,payload: {token}});
 
 //window.store = store;
 const Root  = ()=> (
